10d36f83205a41ac95268b0698d8b63e91c4cd5c
ClaudiaRuizUX <claudiaruizux@gmail.com>
2022-09-30 16:53:44 +1000

    finishes lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'test', to: 'static#test'
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
